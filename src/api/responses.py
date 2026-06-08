938a0973e7cd70cd328a59eb362b112a04638bf4
César Magalhães <cmagalhaes@ciandt.com>
2026-06-08 13:56:29 -0300

    Foi criado o arquivo src/api/responses.py

---
+++ src/api/responses.py
@@ -0,0 +1,11 @@
+﻿"""Respostas HTTP padronizadas da API."""
+
+from __future__ import annotations
+
+from fastapi.responses import JSONResponse
+
+
+class Utf8JSONResponse(JSONResponse):
+    """Resposta JSON com codificacao UTF-8 explicita no Content-Type."""
+
+    media_type = "application/json; charset=utf-8"
